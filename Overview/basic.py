91e154b57e952d780b7a3adbcb537add1c05eb4e
Synaxis <oiacow@gmail.com>
2018-06-08 02:37:56 -0300

    base

---
+++ Overview/basic.py
@@ -0,0 +1,23 @@
+#print("Hello World")
+
+#print("Hello"[0])
+
+# print number (no quotes)
+#print(2)
+
+# print any data type 
+#print(1,2,3,"Hello")
+
+#Data types
+myStr = "Hello"
+myInt = 25
+myFloat = 1.3
+myList = [1,2,3,4,"Hello"]
+
+myDict = {"a": 1, "b": 2, "c": 3}
+
+print(type(myStr), myStr)
+print(type(myInt), myInt)
+print(type(myFloat), myFloat)
+print(type(myList), myList)
+print(type(myDict), myDict)
---
+++ Overview/conditionals.py
@@ -0,0 +1,31 @@
+#conditionals
+
+x = 4
+
+#if else
+
+##if x < 6:
+   ##print("this is true")
+##else:
+   ##print("this is false")
+
+#Elif
+
+color = "red"
+
+if color == "red":
+   print("color is red")
+elif color == "blue":
+   print("color is blue")
+else:
+   print("color is not red or blue")
+
+# Nested if 
+if color == "red":
+   if x < 10:
+      print("color is red and x is less than 10")
+
+#Logical operators
+if color == "red" and x < 10:
+   print("color is red and x is dfjsf")
+

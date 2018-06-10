0b49a9b6c7ed747637f59a524cdef61743c05619
Synaxis <oiacow@gmail.com>
2018-06-10 03:35:14 -0300

    updating

---
+++ Overview/loops.py
@@ -0,0 +1,18 @@
+# Loops
+
+#For Loop
+people = ["John", "Sara", "Tim"]
+for person in people:
+   print("current person: ", person)
+
+#Iteration by seq index
+for i in range(len(people)):
+   print("current person: ",people[i])
+
+for i in rage(0, 10):
+   print(i)
+
+# While Loop
+count = 0
+while count < 10:
+   print("count: ", count)

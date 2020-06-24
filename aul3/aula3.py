148c32beacc0678338713969864ba7b5725939ef
Synaxis <sdjsjahdhjas@gmail.com>
2020-06-23 19:54:18 -0500

    voltando

---
+++ aul3/aula3.py
@@ -0,0 +1,7 @@
+#variaveis
+num1=num2=res=0  #all global variables
+canal="teste"
+
+def cn():
+    print(canal)
+cn()
---
+++ aula2/aula2.py
@@ -0,0 +1,6 @@
+num=1
+num="Bruno"
+num=1.5
+print(num)
+
+#the last added value is the one to be printed

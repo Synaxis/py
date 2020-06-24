750dac4b29f20cfdf6111a992a2f24df014c2e5d
Synaxis <sdjsjahdhjas@gmail.com>
2020-06-23 20:29:08 -0500

    a

--- aula5/aula5.py
+++ aula5/aula5.py
@@ -1,9 +1,17 @@
+import random
 #tipos numericos, random e operações de casting
 num_i=10
 num_f=5.2
 num_c=1j
 
 x=num_i
+
+num=[0,1,2,3]
+print(num)
 #usamos o str para converter o inteiro para uma string
+# e assim por diante bem facil é so colocar o tipo antes da dado
 
 print("Valor da variavel: " + str(x) + "- Tipo: " + str(type(x)))
+
+x="isso é uma string"
+print(type(x))

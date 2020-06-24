4877304942b855e7d345d23b4d7a5797e1950f5e
Synaxis <sdjsjahdhjas@gmail.com>
2020-06-23 20:17:56 -0500

    a

---
+++ aula4/aula4.py
@@ -0,0 +1,6 @@
+#tipos de dados em python
+x=1 
+x="Cursos" #string
+
+print(x)
+print(type(x))
---
+++ aula5/aula5.py
@@ -0,0 +1,9 @@
+#tipos numericos, random e operações de casting
+num_i=10
+num_f=5.2
+num_c=1j
+
+x=num_i
+#usamos o str para converter o inteiro para uma string
+
+print("Valor da variavel: " + str(x) + "- Tipo: " + str(type(x)))

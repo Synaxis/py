02d80602049b29439882780a4ef573c05221f1b2
Synaxis <sdjsjahdhjas@gmail.com>
2020-06-24 23:37:57 -0500

    update ate aula9

---
+++ aula8/aula8.py
@@ -0,0 +1,15 @@
+import sys
+
+'''bool é true ou false
+tem que ser escrito em caps'''
+aula=True
+
+print(aula)
+
+num1=2
+num2=4
+
+if num1>num2:
+    print("numero 1 é maior que numero2")
+else:
+    print("numero1 é MENOR que numero2")
---
+++ aula9/aula9.py
@@ -0,0 +1,12 @@
+#Colecao list
+carros=["HRV","Golf","Argo","Focus"]
+
+
+print(carros) #imprime a lista inteira
+
+print(carros[0]) #imprime o item no indice 0
+
+#altera o valor no indice indicado pela atribuição
+carros[3]="NovoCarro"
+
+print("a nova lista de carros eh:",carros)

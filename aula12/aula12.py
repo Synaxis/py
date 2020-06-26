ecf026edb77b0b3ee6393dbd1f3f1d20c22cd6e2
Synaxis <sdjsjahdhjas@gmail.com>
2020-06-26 02:18:58 -0500

    aula12

---
+++ aula12/aula12.py
@@ -0,0 +1,10 @@
+#loops
+carros=["HRV", "Golf", "Argo", "Focus"]
+
+for x in carros: # inicializa sem parentes e ja criamos uma variavel dentro do for
+    print(x)
+    if(x=="Golf"):
+        print("VW")
+    #podemos ja usar 
+# for x ( ja cria a variavel)
+# for x in (ja mostra qual array percorrer)
